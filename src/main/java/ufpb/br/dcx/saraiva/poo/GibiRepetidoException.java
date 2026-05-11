087aac707f9a63e9c6a4581eb207896c100fd63d
gabriel.lima <saraivagabriel130@gmail.com>
2026-05-11 11:38:53 -0300

    criação da classe Exeception e interface

---
+++ src/main/java/ufpb/br/dcx/saraiva/poo/GibiRepetidoException.java
@@ -0,0 +1,7 @@
+package ufpb.br.dcx.saraiva.poo;
+
+public class GibiRepetidoException extends Exception{
+    public GibiRepetidoException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/ufpb/br/dcx/saraiva/poo/ProgramaGibi.java
@@ -0,0 +1,5 @@
+package ufpb.br.dcx.saraiva.poo;
+
+public interface ProgramaGibi {
+
+}
