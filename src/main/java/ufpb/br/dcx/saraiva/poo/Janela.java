4db7060c9029542a125cd2665396a3d777595be6
saraiva <saraivagabriel130@gmail.com>
2026-05-25 22:17:37 -0300

    mudando o layout para flow

--- src/main/java/ufpb/br/dcx/saraiva/poo/Janela.java
+++ src/main/java/ufpb/br/dcx/saraiva/poo/Janela.java
@@ -1,6 +1,7 @@
 package ufpb.br.dcx.saraiva.poo;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,6 +10,7 @@ public class Janela {
         JFrame window = new JFrame("Home");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setSize(300, 300);
+        window.setLayout(new FlowLayout());
 
         Colecao cole = new Colecao();
         Gibi abobra = new Gibi("1", "Reino do ontem", "superman", "batman é bom demais", 9);
